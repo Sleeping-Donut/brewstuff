dc45064fdab0ec23fde406ae9d275aa60b332987
Sleeping-Donut <nathandavis1999@gmail.com>
2024-08-29 17:05:11 +0100

    update zen browser cask

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -1,9 +1,9 @@
 cask "zen-browser" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.0.0-a.29"
-  sha256 arm:   "b9892f13bb1c8cd0c6ab0de73f923641ca0cce1bd68ecffaf683fd01fdd30251",
-         intel: "934b294ba30586a31c2c62e88ef95c42312f9e0172e9977d495c909934d5ba25"
+  version "1.0.0-a.32"
+  sha256 arm:   "3b9ab978317a780f2f8f23976f95f190dafc258144cbfe9e45dba6589aad5faa",
+         intel: "defde5572cb6310c57dbfb09f1e9bd00283498b538623545b797096b2b1bc7a7"
 
   url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg",
       verified: "github.com/zen-browser/desktop/"
@@ -13,14 +13,19 @@ cask "zen-browser" do
 
   livecheck do
     url :url
-    strategy :github_latest
+    strategy :github_latest do |json|
+      json["tag_name"].match(/^v?(\d+(?:\.\d+)+(-a\.\d+)?)$/i)[0]
+    end
   end
+
+  depends_on macos: ">= :catalina"
 
   app "Zen Browser.app"
 
   zap trash: [
     "~/Library/Application Support/zen",
     "~/Library/Caches/zen",
+    "~/Library/Caches/Mozilla/updates/Applications/Zen Browser/",
     "~/Library/Preferences/org.mozilla.com.zen.browser.plist",
     "~/Library/Saved Application State/org.mozilla.com.zen.browser.savedState",
   ]
@@ -28,6 +33,7 @@ cask "zen-browser" do
   caveats <<~EOS
     According to https://github.com/zen-browser/desktop/issues/53
     this app will not work with quarantine attributes.
+    Bypass gatekeeper: 'xattr -d com.apple.quarantine '/Applications/Zen Browser.app/'
   EOS
 end
 
